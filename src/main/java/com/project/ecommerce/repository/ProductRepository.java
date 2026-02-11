c428b1e70b22023bd37843998b9c424b612203bb
Saniya Pathan <saniyapathan9175@gmail.com>
2026-02-11 11:24:20 +0530

    Updated Code

--- src/main/java/com/project/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/project/ecommerce/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package com.project.ecommerce.repository;
 
 import com.project.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findBySellerId(int sellerId);
+
+    List<Product> findBySeller_SellerId(int sellerId);
+
 }
